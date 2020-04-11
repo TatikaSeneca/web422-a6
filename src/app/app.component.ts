81ed3f7adeddc2415f2827367750041294821bd2
Tatika Anderson <tanderson13@myseneca.ca>
2020-04-11 02:05:20 -0400

    add online link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@
 *
 * Name: Tatika Anderson     Student ID: 123 229 171       Date: 10-Apr-2020 
 * 
-* Online Link: _______________________________________________________________
+* Online Link: https://sharp-murdock-c7c945.netlify.com
 * ********************************************************************************/
 
 import { Component } from '@angular/core';
